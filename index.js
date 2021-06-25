47b9431c6ba8b228e1b913bfbf225eb119c45040
Luis Pablo <wichos11.ch@gmail.com>
2021-06-25 01:34:48 -0600

    creando pagina inicio de datos

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+
+app.get('/', (req, res) => {
+
+    res.send("Luis Hernandez 201504497")
+})
+
 app.get('/', (req, res) => {
     // obtiene los datos
   let turno = req.query.turno;
